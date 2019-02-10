d159fdcbcef55d77bdd8a804e0128f8268b20c12
Russell A Epstein <rtr1e@wakefern.com>
2019-02-10 12:28:54 -0500

    Trying to get email working

--- email.php
+++ email.php
@@ -3,7 +3,6 @@ require("sendgrid-php/sendgrid-php.php");
 
 
 $subject = $_POST['Subject'];
-echo $subject;
 
 $request_body = json_decode('{
   "personalizations": [
@@ -13,7 +12,7 @@ $request_body = json_decode('{
           "email": "repstein95@gmail.com"
         }
       ],
-      "subject": ' $subject '
+      "subject": "' . $subject . '"
     }
   ],
   "from": {
