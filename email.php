bdb627135467e21f8d85e221ef13a1cf4ab09883
Russell A Epstein <rtr1e@wakefern.com>
2019-02-10 10:34:27 -0500

    Trying to get email working

--- email.php
+++ email.php
@@ -1,23 +1,33 @@
 <?php 
-if(isset($_POST['submit'])){
-    $to = "mrgndwnn@gmail.com"; // this is your Email address
-    $from = $_POST['Email']; // this is the sender's Email address
-    $name = $_POST['Name'];
-    $subject = $_POST['Subject'];
-    $message = $name . " wrote the following:" . "\n\n" . $_POST['Message'];
-    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
+require("sendgrid-php/sendgrid-php.php");
 
-	$header.= "MIME-Version: 1.0\r\n"; 
-	$header.= "Content-Type: text/html; charset=ISO-8859-1\r\n"; 
-	$header.= "X-Priority: 1\r\n"; 
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    $status = mail($to,$subject,$message,$headers);
-    mail($from,$subject,$message2,$headers2); // sends a copy of the message to the sender
-    if($status)
-	{ 
-		echo '<p>Your mail has been sent!</p>';
-	} else { 
-		echo '<p>Something went wrong, Please try again!</p>'; 
-	}
+$request_body = json_decode('{
+  "personalizations": [
+    {
+      "to": [
+        {
+          "email": "repstein95@gmail.com"
+        }
+      ],
+      "subject": "Hello World from the SendGrid PHP Library!"
+    }
+  ],
+  "from": {
+    "email": "repstein95@gmail.com"
+  },
+  "content": [
+    {
+      "type": "text/plain",
+      "value": "Hello, Email!"
+    }
+  ]
+}');
+
+$apiKey = getenv('SENDGRID_API_KEY');
+$sg = new \SendGrid($apiKey);
+
+$response = $sg->client->mail()->send()->post($request_body);
+echo $response->statusCode();
+echo $response->body();
+echo $response->headers();
 ?>
